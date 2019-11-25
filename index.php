fe2709f3bd80ca4825027e9aac7aa0c9374459c0
Leo310 <leonardheininger1@gmail.com>
2019-11-25 23:19:08 +0100

    testing php

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
-<!DOCTYPE html>
-<html lang="de">
-<head>
-<meta charset="utf-8">
-<title>Start</title>
-</head>
+ <?php
+include ('includes/header.html');
+?>
+
+</head>   
 
 <body>
 	<h1>Meine Webside</h1>
-	<p>testing script and keybind</p>
+	<p>testing script and keybind joooo</p>
 	<a href="cam"><button>Kamera</button></a>
 	<form action="scr_aus.php" method="post">
 		<input type="submit" name="aus" value="ausschalten">
