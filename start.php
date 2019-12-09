672da6220d3854974541c17191bd5840fdab32dc
Pi <leonardheininger1@gmail.com>
2019-12-09 21:44:18 +0100

    hoffentlich jetzt

--- start.php
+++
@@ -1,9 +0,0 @@
-<?php
- session_start();
- // Umleitung bei Aufruf ohne Anmeldung
- if (!isset($_SESSION['user'])) header("location:index.php");
- include("include/inc_htmlhead");
- include('include/inc_htmlkopfzeile.php');
-?>
-</body>
-</html>
